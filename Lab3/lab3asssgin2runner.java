71b3521e82102c8dffb67486a8c39ad17ccf7ce6
Shaharyar07 <70062821+Shaharyar07@users.noreply.github.com>
2021-03-07 19:50:12 +0500

    lab assignments

--- Lab3/lab3asssgin2runner.java
+++ Lab3/lab3asssgin2runner.java
@@ -2,13 +2,20 @@
 public class lab3asssgin2runner {
 
 	public static void main(String[] args) {
-		time user = new time();
-		user.hours = 6;
-		user.mint = 11;
-		user.sec = 33;
-		user.Display();
-		time user1 = new time(11,33,22);
-		user1.Display();
+		  Time user = new Time();
+	        user.display();
+	        user.setHours(user.hours);
+	        user.setMin(user.minutes);
+	        user.setSec(user.seconds);
+
+
+	        Time user1 = new Time(4,17,61);
+	        user1.display();
+	        user1.setHours(user1.hours);
+	        user1.setMin(user1.minutes);
+	        user1.setSec(user1.seconds);
+
+
 
 	}
 
--- Lab3/time.java
+++ Lab3/time.java
@@ -1,18 +1,37 @@
 
 
-public class time {
-	int hours,mint,sec;
-	public time() {
-		 hours = hours;
-		 mint = mint;
-		 sec =sec;
-	}
-	public time( int hours,int mint,int sec) {
-		this.hours = hours;
-		this.mint = mint;
-		this.sec = sec;
-	}
-	void Display(){
-		System.out.println("Time is: "+hours+":"+mint+":"+sec);
-	}	
+public class Time {
+    int hours;
+    int minutes;
+    int seconds;
+
+    public Time(){
+        hours=7;
+        minutes=13;
+        seconds=30;
+    }
+    public Time(int hr , int min , int sec){
+        hours=hr;
+        minutes=min;
+        seconds=sec;
+    }
+    public void setHours(int hours){
+        if (hours>24){
+            System.out.println("Invalid hour!!!!");
+        }
+
+    }
+    public void setMin(int minutes){
+        if (minutes>60){
+            System.out.println("Invalid minutes!!!");
+        }
+    }
+    public void setSec(int seconds){
+        if (seconds>60){
+            System.out.println("Invalid seconds");
+        }
+    }
+    public void display(){
+        System.out.println(hours +" : " + minutes + " : " + seconds);
+    }
 }
