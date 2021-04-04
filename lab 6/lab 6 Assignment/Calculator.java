097843fc7cf013168b4a6f2444ebb1508d23f9d4
Shaharyar07 <70062821+Shaharyar07@users.noreply.github.com>
2021-04-04 23:31:09 +0500

    lab 6 Assignment

---
+++ lab 6/lab 6 Assignment/Calculator.java
@@ -0,0 +1,42 @@
+
+public class Calculator {
+	public static double sum(double a,double b) {
+		double sum = a + b;
+		return sum;
+		
+	}
+
+	public static double multiply(double i, double j) {
+		double multiply = i * j;
+		return multiply;
+		
+	}
+	
+	public static double divide(double x, double y) {
+		double divide = x / y;
+		return divide;
+		
+	}
+	
+	public static double modulus(double x, double y) {
+		double mod = x % y;
+		return mod;
+		
+	}
+	
+	public static double sin(double a) {
+		return Math.sin(a);
+		
+	}
+	
+	public static double cos(double a) {
+		return Math.cos(a);
+		
+	}
+
+	public static double tan(double a) {
+		return Math.tan(a);
+	}
+	
+
+}
---
+++ lab 6/lab 6 Assignment/CalculatorRunner.java
@@ -0,0 +1,14 @@
+
+public class CalculatorRunner {
+
+	public static void main(String[] args) {
+		System.out.println(Calculator.sum(30, 22));
+		System.out.println(Calculator.multiply(5,6));
+		System.out.println(Calculator.divide(10, 3));
+		System.out.println(Calculator.modulus(10, 2));
+		System.out.println(Calculator.sin(30));
+		System.out.println(Calculator.cos(30));
+		System.out.println(Calculator.tan(30));
+		
+	}
+}
